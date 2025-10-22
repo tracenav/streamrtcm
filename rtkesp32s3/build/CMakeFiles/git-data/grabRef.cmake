b18100ad782955e5962be3f36654858a3a2eb105
aUsernamePNG <alpha228@protonmail.com>
2025-10-22 16:37:29 -0400

    big cleanup

--- rtkesp32s3/build/CMakeFiles/git-data/grabRef.cmake
+++ rtkesp32s3/build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/Users/delta/streamrtcm/rtkesp32s3/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/Users/delta/esp/v5.5/esp-idf/.git")
+set(GIT_DIR "/Users/delta/streamrtcm/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
